ebe404e31206d815ded4330a9dbff05d1bc7f89a
CrazyDevpper <44318032+CrazyDevpper@users.noreply.github.com>
2018-10-20 15:34:15 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,6 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-
-
+const ffmpeg = require("ffmpeg");
 
 client.on('ready',async () => {
   client.channels.find(ch => ch.id === "503095644081356810" && ch.type === 'voice').join();
